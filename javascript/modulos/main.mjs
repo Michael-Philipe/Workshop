9e852842756cfc375000dda866478602303a802d
Michael <michael.philipe2013@gmail.com>
2022-05-02 16:42:55 -0300

    trabalhando com modulos

---
+++ javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraHobbie, mostraIdade } from './funcoes.mjs';
+
+console.log(mostraIdade('michael', 25));
+console.log(mostraCidade('michael', 'minas gerais'));
+console.log(mostraHobbie('michael', 'adquirir recurso'));
